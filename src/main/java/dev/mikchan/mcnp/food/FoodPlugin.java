7eb34b7e4788fd291ea27ab08e83fba29c785d5e
wtlgo <georgeendo0@gmail.com>
2022-07-14 02:42:41 +0300

    Initial commit

---
+++ src/main/java/dev/mikchan/mcnp/food/FoodPlugin.java
@@ -0,0 +1,18 @@
+package dev.mikchan.mcnp.food;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public final class FoodPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
