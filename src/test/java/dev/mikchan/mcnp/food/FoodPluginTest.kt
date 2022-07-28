13d7a077356282f5bb2c9f8d7ddcaf94dc81676e
wtlgo <georgeendo0@gmail.com>
2022-07-28 18:17:22 +0300

    This doesn't work, huh?

--- src/test/java/dev/mikchan/mcnp/food/FoodPluginTest.kt
+++ src/test/java/dev/mikchan/mcnp/food/FoodPluginTest.kt
@@ -2,10 +2,9 @@ package dev.mikchan.mcnp.food
 
 import be.seeseemelk.mockbukkit.MockBukkit
 import be.seeseemelk.mockbukkit.ServerMock
-import kotlin.test.AfterTest
-import kotlin.test.BeforeTest
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import kotlin.test.*
 
 internal class FoodPluginTest {
     lateinit var server: ServerMock
@@ -25,5 +24,22 @@ internal class FoodPluginTest {
     @Test
     fun testDefaultConfigValues() {
         assertEquals(false, plugin.config.enabled, "The plugin should be disabled by default.")
+    }
+
+    @Test
+    fun testPlayerEatingSweetBerriesWhileConfigIsDisabled() {
+        assertEquals(false, plugin.config.enabled, "The plugin should be disabled by default.")
+
+        val player = server.addPlayer("Consoomer")
+        player.foodLevel = 0
+        player.saturation = 0f
+
+        assertEquals(0, player.foodLevel, "${player.name} should be hungry.")
+
+        val sweetBerries = ItemStack(Material.SWEET_BERRIES)
+        sweetBerries.amount = 1
+        player.simulateConsumeItem(sweetBerries)
+
+        assertNotEquals(0, player.foodLevel, "${player.name} should not be hungry.")
     }
 }
